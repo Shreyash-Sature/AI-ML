da58c50525e0671d4523b390e5844afeaf4136ff
Shreyash Sature <shreyashsature@gmail.com>
2025-11-16 11:43:03 +0530

    Added Input File

---
+++ Python Fundamentals/Input.py
@@ -0,0 +1,7 @@
+username = input("Enter your username : ")
+print("Welcome ", username)
+
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+sum = a + b
+print ("Sum of ",a," and ", b, " is : ",sum)
