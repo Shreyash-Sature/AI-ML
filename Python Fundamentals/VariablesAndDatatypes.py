407324afc211dfa1fdd7e6e00950a52dbeb71df8
Shreyash Sature <shreyashsature@gmail.com>
2025-11-16 10:21:11 +0530

    Added Variables And Datatypes

---
+++ Python Fundamentals/VariablesAndDatatypes.py
@@ -0,0 +1,40 @@
+name = "Shreyash"
+age = 20
+PI = 3.14 
+
+print(type(name)) #gives data type of name
+
+a = 10
+b = 12
+sum = a + b
+print(sum)
+
+"""
+Python is a case-sensitive language
+Indentations are important
+- Identifiers : Name of variable, Function, etc
+  Rule for Idententifiers : Should start with English alphabets or "_" and with any other symbol or Digit.
+
+DATA TYPES :
+- Integer : +ve, -ve, 0
+- String : ' ', " ", "' '"
+- Float : decimal numbers
+- Boolean : True, False
+- None
+
+
+KEYWORDS : 
+Reserved word that have fixed meaning and can not be used as Identifiers(name of any function, variable etc)
+
+COMMENTS:
+# for single line
+"""  """ for multiline comment 
+
+STYLE GUIDE:
+use snake is prefered in python
+-snake case : roll_no
+-camel case : rollNo
+-Pascal case : RollNo
+
+
+"""
