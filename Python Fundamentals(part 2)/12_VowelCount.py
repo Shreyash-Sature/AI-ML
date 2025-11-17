a6cecc7397115abc18361d53bb3562644beb252a
Shreyash Sature <shreyashsature@gmail.com>
2025-11-17 16:28:32 +0530

    Path changed

---
+++ Python Fundamentals(part 2)/12_VowelCount.py
@@ -0,0 +1,7 @@
+word = "artificial"
+count =0
+
+for ch in word:
+    if(ch == 'a' or ch=='e' or ch=='i' or ch=='o' or ch =='u'):
+        count = count +1
+print("numbers of vowels in given word are :",count)
