3610c9950d99cfd2c5d5c52a3ea92cc9b45c33b6
Shreyash Sature <shreyashsature@gmail.com>
2025-11-18 23:25:32 +0530

    Lambda Function

---
+++ Python Fundamentals(part 2)/18_LambdaFunction.py
@@ -0,0 +1,14 @@
+"""lambda function :
+it does not need a name.
+It is mainly used when you need a short function for a short time.
+    
+lambda arguments/parameters(a,b,c): expression
+
+lambda → keyword
+arguments → input values (like function parameters)
+expression → the result returned (must be a single expression)"""
+
+ 
+sum = lambda a,b : a+b
+print(sum(4,5))
+
