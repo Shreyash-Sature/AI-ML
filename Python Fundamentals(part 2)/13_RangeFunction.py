d9bbdcfbdde70ea3bc0134d3dc41ef2779496a2d
Shreyash Sature <shreyashsature@gmail.com>
2025-11-18 23:00:50 +0530

    Practice Questions

---
+++ Python Fundamentals(part 2)/13_RangeFunction.py
@@ -0,0 +1,8 @@
+""" range()
+        generates sequence 
+
+    -range(5) => 0 to 4
+
+    -range(start, stop, step)
+    eg. range(1,10,2) => 1 3 5 7 9 
+"""
---
+++ Python Fundamentals(part 2)/16_PracticeExamples.py
@@ -0,0 +1,8 @@
+# Calculate average
+
+
+def avg (a,b,c):
+    av = (a+b+c)/3
+    return av
+ans = avg(3,4,5)
+print("Average of a,b,c is :",ans)
