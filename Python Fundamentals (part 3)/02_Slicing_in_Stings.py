5ccf7ebf1454287112b69a471d35e888b5a01c2f
Shreyash Sature <shreyashsature@gmail.com>
2025-11-22 15:13:13 +0530

    Slicing in Strings

---
+++ Python Fundamentals (part 3)/02_Slicing_in_Stings.py
@@ -0,0 +1,19 @@
+"""
+Slicing : Extracting a part (substring) from a string.
+
+Syntax : string[start : stop : step]
+
+"""
+"""
+Ways to print whole string:
+-print (string[:]
+-print (string [:len(string)])
+-print (string [0:])
+
+"""
+
+word = "Shreyash"
+print(word[4:8:1])
+
+line = "Hi I am Shreyash Sature"
+print(line[8:])
