d3b58e2febb32f89e37904c1d01236e8d3233377
Shreyash Sature <shreyashsature@gmail.com>
2025-11-22 15:32:38 +0530

    String Formatting

---
+++ Python Fundamentals (part 3)/03_Formatting_String.py
@@ -0,0 +1,32 @@
+"""
+String Formatting : Inserting variables into strings in a clean way.
+
+"""
+"""
+1) F-String Formatting : literal string interpolation (advanced)
+
+Synatx :
+f "{a}"
+"""
+
+a = 5
+b =10
+print(f"Sum of {a} and {b} is {a+b}")
+
+
+
+
+"""a = 5
+b = 10
+sum = a+b
+
+# Normal formatting
+print("Sum of {} and {} is : {}".format(a,b,sum))
+
+#index based formatting
+print("Sum of {1} and {0} is : {2}".format(a,b,sum))
+
+#value based formatting
+print("Sum of {a} and {b} ".format(a=5,b=10))
+"""
+
