2310c00af734b482fc934f9b526d07f18e66a5a5
Shreyash Sature <shreyashsature@gmail.com>
2025-11-23 08:57:10 +0530

    Tuple Methods

--- 03_Python Fundamentals (part 3)/07_Tuples_in_Python.py
+++ 03_Python Fundamentals (part 3)/07_Tuples_in_Python.py
@@ -11,7 +11,15 @@ Syntax : tuple_name = (el1, el2, el3)
 # print(tup)
 
 tup = (1,2,3,4,5)
+
+#indexing and slicing
+print(tup[2])
+print(tup[1:4])
+
+#Loops on tuple
 sum =0
 for var in tup :
     sum += var
 print(f"Sum of elements in tuple is {sum} .")
+
+
---
+++ 03_Python Fundamentals (part 3)/08_Tuple_Methods.py
@@ -0,0 +1,9 @@
+"""
+Tuple Methods :
+1) tup.index(elmnt of tup) : returns INDEX of first occuring element
+2)tup.count(elmnt of tuple) : return total count of occurance of that element.
+"""
+
+tup = (22,11,3,23,11,45,76,87,23)
+print(tup.count(11))
+print(tup.index(23))
