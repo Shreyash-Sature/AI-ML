1be0852de7ff8ad5de6ac2526a34130dd4c2bf54
Shreyash Sature <shreyashsature@gmail.com>
2025-11-24 23:46:01 +0530

    OOP

---
+++ 04_Python Fundamentals(part 4)/01_OOP.py
@@ -0,0 +1,17 @@
+"""
+Object Oriented Programming : Organization of code using classes and objects.
+
+-Class
+A class is a blueprint/template for creating objects.
+class Student:
+    name = "Shreyash"
+
+-Object
+
+Object is an instance of a class.
+
+s1 = Student()
+print(s1.name)
+
+
+"""
