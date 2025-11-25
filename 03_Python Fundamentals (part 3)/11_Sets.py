8c56cd1c8b87c913395a1e4ae2f290a8dd276aa0
Shreyash Sature <shreyashsature@gmail.com>
2025-11-25 12:10:38 +0530

    Sets

--- 03_Python Fundamentals (part 3)/11_Sets.py
+++ 03_Python Fundamentals (part 3)/11_Sets.py
@@ -1,9 +1,15 @@
 """
 Sets : Collection of unique elements
 doesnt print similar or duplicate elements
+
+empty set is created by 
+abc = set()
+
+and not by
+abc ={ }  --- this creats a empty dictionary
 """
 
 set1 = {1,2,3,3,4,5,2,2}
 print(set1)
 
-print(len(set1))
+print(len(set1)) #=> 5 not 8
