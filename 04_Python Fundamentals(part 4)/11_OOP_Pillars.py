cb35fac87df68382621c760c64715f7a3ef8a597
Shreyash Sature <shreyashsature@gmail.com>
2025-11-28 23:22:47 +0530

    Four Pillars of OOP

---
+++ 04_Python Fundamentals(part 4)/11_OOP_Pillars.py
@@ -0,0 +1,6 @@
+"""
+OOP Pillars :
+1)Encapsulation : Wrapping data + methods into a class and restricting direct access to data.
+2)Abstraction : Hiding unnecessary internal details and showing only required features.
+3)Inheritance : One class inherits (reuses) methods and variables of another class.
+4)Polymorphism : Same function name → different behavior in different classes."""
