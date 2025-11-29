e4cbdb2bc29161496e85b192c541b574cbdeb765
Shreyash Sature <shreyashsature@gmail.com>
2025-11-29 23:41:03 +0530

    Encapsulation

--- 04_Python Fundamentals(part 4)/12_Encapsulation_in_OOP.py
+++ 04_Python Fundamentals(part 4)/12_Encapsulation_in_OOP.py
@@ -1,1 +1,24 @@
 #Encapsulation : Wrapping data + methods into a single unit (class) and restricting direct access to data.
+"""
+Three levels of data hiding:
+i)Public : Attributes that are accessible inside and outide of class
+Syntax :(variable)
+
+ii)Protected: accessible in class and subclass
+Syntax :(_variable)
+
+iii)Private: accessible only in class
+Syntax :(__variable)
+getters and setters(functions) are used to access and update private variables safely
+"""
+
+class Bank:
+    def __init__(self,name,balance):
+        self.name = name
+        self.__balance = balance
+
+    def get_info(self):
+        print(f"Name is : {self.name} and acc balance is {self.__balance}")
+
+cust1 = Bank("Shreyash",111_01_00_000)
+cust1.get_info()
