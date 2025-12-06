9a5a48be7d750a6348ba436a07604f28c54b6cf5
Shreyash Sature <shreyashsature@gmail.com>
2025-12-06 14:01:39 +0530

    Encapsulation

--- 04_Python Fundamentals(part 4)/12_Encapsulation_in_OOP.py
+++ 04_Python Fundamentals(part 4)/12_Encapsulation_in_OOP.py
@@ -9,18 +9,27 @@ Syntax :(_variable)
 
 iii)Private: accessible only in class
 Syntax :(__variable)
+
 getters and setters(functions) are used to access and update private variables safely
 """
 
-class Bank:
+class BankAccount:
     def __init__(self,name,balance):
-        self.name = name
-        self.__balance = balance
+        self.name = name  #public attribute
+        self.__balance = balance   #private attribute - data mangling
 
-    def get_info(self):
-        print(f"Name of account holder is {self.name} and acc balance is {self.__balance}")
+    def get_balance(self): #getter function
+        return self.__balance
+    
+    def set_balance(self,newBalance): #setter function to update the private attribute
+        self.__balance = newBalance
+        
+    
+    
+acc1 = BankAccount("Shreyash",100000)
+acc2 = BankAccount("Shreya", 1000)
 
-cust1 = Bank("Shreyash",111_01_00_000)
-cust2 = Bank("Shreya",1_00_000)
-cust1.get_info()
-cust2.get_info()
+print(acc1.name, acc1.get_balance())
+
+acc1.set_balance(200000) #to update the private attribute (balance)
+print(acc1.name, acc1.get_balance())
