7559269dd46b6d1281c7f1b86cfed6877ce3b618
Shreyash Sature <shreyashsature@gmail.com>
2025-12-06 22:40:55 +0530

    Duck Typing

---
+++ 04_Python Fundamentals(part 4)/17_Polymorphism(Duck_Typinig).py
@@ -0,0 +1,18 @@
+"""
+Duck Typing : the type or class of an object is not important —
+              the behavior (methods/properties) matters.
+"""
+
+class Teacher:
+    def get_designation(self):
+        print("Designation = Teacher")
+
+class Accountant:
+    def get_designation(self):
+        print("Designation = Accountant")
+
+t1 = Teacher()
+t1.get_designation()
+
+acc1 = Accountant()
+acc1.get_designation()
