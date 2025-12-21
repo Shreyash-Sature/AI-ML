a318ee657746d0bf3631068c8fc6d3b6e80f52ce
Shreyash Sature <shreyashsature@gmail.com>
2025-12-22 00:10:59 +0530

    JSON Module

---
+++ 05_Python Fundamentals (part 5)/10_JSON_Module.py
@@ -0,0 +1,42 @@
+"""
+JSON (Java script object notation) : 
+
+json.dumps(): Python → JSON (String)
+json.loads() : JSON(Strings) → Python
+
+
+"""
+import json
+
+
+#for strings
+json_str = '{"name" : "Shreyash", "isStudent" : true}'
+py_obj = json.loads(json_str)   #JSON to Python (Strings)
+print(py_obj, type(py_obj))
+
+py_obj = {
+    "name " : "Shreyash",
+    "isStudent" : True
+} 
+json_obj = json.dumps(py_obj)     #Python to JSON (Strings)
+print(json_obj, type(json_obj))
+
+
+#for files 
+#loads JSON info to Python
+"""with open("10_data.json","r") as f:
+    py_obj = json.load(f)
+
+    print(py_obj,type(py_obj))"""
+
+
+#dumps Python info to JSON file
+
+data ={
+    "name" : "Shreyash",
+    "isStudent":True,
+    "age" : 21
+}
+
+with open("10_data.json","w") as f:
+    json.dump(data,f, indent=4, sort_keys=True)
