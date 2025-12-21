910b8ca56ddaee95b5b3ce7af72ca7a633e543ec
Shreyash Sature <shreyashsature@gmail.com>
2025-12-21 15:21:20 +0530

    with Keyword in File Handling

---
+++ 05_Python Fundamentals (part 5)/04_with_Keyword.py
@@ -0,0 +1,16 @@
+"""
+"with" keyword in file handling : 
+                                  It ensures that a resource (like a file) is 
+                                  properly opened and closed, even if an error occurs.
+Normally, we write:
+
+f = open("data.txt", "r")
+print(f.read())
+f.close()
+
+If an error occurs before f.close(), the file remains open
+Can cause memory leaks
+"""
+
+with open("data.txt","r") as f:
+    print(f.read())
