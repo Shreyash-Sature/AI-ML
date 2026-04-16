cb7b0187cfdf403d25a1536ddf3c839a4a94bdb1
Shreyash Sature <shreyashsature@gmail.com>
2026-04-16 07:52:18 +0530

    SQL Queries

---
+++ 10_SQL/02_DataBase_Queries.sql
@@ -0,0 +1,17 @@
+-- DATABASE Queries
+
+-- 1. CREATE DATABASE (USED TO CREATE NEW DATABASE)
+CREATE DATABASE db_name;
+CREATE DATABASE IF NOT EXISTS db_name;
+
+-- 2. USE (specifies which database to use)
+USE db_name;
+
+-- 3. SHOW DATABASE (shows all databases)
+SHOW DATABASES;
+
+-- 4. SHOW TABLES (shows tables in database)
+SHOW TABLES;
+
+-- 4. DROP DATABASE (used to delete database)
+DROP DATABASE db_name;
